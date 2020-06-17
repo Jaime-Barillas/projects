3f147a1f27f877d634ecb395b50b62fc9db64b16
Jaime Barillas <2942256-JBarillas@users.noreply.gitlab.com>
2020-06-17 02:37:34 -0400

    Add MenuBar and StatusBar (no functionality)

--- src/mapmaker/views.clj
+++ src/mapmaker/views.clj
@@ -2,6 +2,33 @@
   (:import
     (javafx.stage Screen)))
 
+(def menus
+  [{:fx/type :menu
+    :text "File"
+    :items [{:fx/type :menu-item
+             :text "New"}
+            {:fx/type :menu-item
+             :text "Open..."}
+            {:fx/type :menu-item
+             :text "Save"}
+            {:fx/type :menu
+             :text "Export As"
+             :items [{:fx/type :menu-item
+                      :text "PNG"}
+                     {:fx/type :menu-item
+                      :text "JPG"}]}]}
+   {:fx/type :menu
+    :text "Help"
+    :items [{:fx/type :menu-item
+             :text "About"}]}])
+
+(defn status-bar [{:keys [map-name]}]
+  {:fx/type :h-box
+   :alignment :center-left
+   :spacing 5
+   :children [{:fx/type :label
+               :text (or map-name "No Active Map")}]})
+
 (defn root-view [{:keys [state]}]
   (let [bounds (.getVisualBounds (Screen/getPrimary))
         x (.getMinX bounds)
@@ -16,5 +43,8 @@
      :min-width 480
      :min-height 360
      :scene {:fx/type :scene
-             :root {:fx/type :label
-                    :text "FIXME"}}}))
+             :root {:fx/type :border-pane
+                    :top {:fx/type :menu-bar
+                          :menus menus}
+                    :bottom {:fx/type status-bar
+                             :map-name (:map-name state)}}}}))
