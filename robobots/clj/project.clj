7541d981655c235f7d6f4ddacef5e42286f90245
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2022-09-05 23:08:42 -0400

    robobots(clj): Add httpkit and reitit deps

--- robobots/clj/project.clj
+++ robobots/clj/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "ISC"
             :url "https://opensource.org/licenses/ISC"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [http-kit/http-kit "2.6.0"]
+                 [metosin/reitit-ring "0.5.18"]]
   :main ^:skip-aot robobots.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- robobots/clj/src/robobots/core.clj
+++ robobots/clj/src/robobots/core.clj
@@ -1,7 +1,24 @@
 (ns robobots.core
+  (:require
+    [org.httpkit.server :as server]
+    [reitit.ring :as ring])
   (:gen-class))
+
+(defn- handler [req]
+  (let [params (:path-params req)]
+    {:status 200
+     :headers {"Content-Type" "text/plain"}
+     :body (str "You requested the contents of the " (:table-name params) " table.")}))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      ["/table/:table-name" handler])))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (server/run-server
+    app
+    {:port 8080
+     :legacy-return-value? false}))
